0dd1a8e1daa6de31e8d5dfc36cd8c488e343a9f9
Nemanja Lepojevic <nemanja_94@live.com>
2020-02-03 17:09:20 +0100

    Check in

--- AerConnect/Controllers/CheckInController.cs
+++ AerConnect/Controllers/CheckInController.cs
@@ -8,10 +8,42 @@ namespace AerConnect.Controllers
 {
     public class CheckInController : Controller
     {
+        AvioKompanijaEntities1 entities;
         // GET: CheckIn
         public ActionResult Index()
         {
+            entities = new AvioKompanijaEntities1();
+            return View("Checkin");
+        }
+
+        public ActionResult CheckIn()
+        {
             return View();
         }
-    }
-}
+
+        [HttpPost]
+        public ActionResult CheckIn(int brojPasosa, int sifraRezervacije)
+        {
+            if(ModelState.IsValid)
+            {
+                CheckIn novi = new CheckIn()
+                {
+                    BrojPasosa = brojPasosa,
+                    SifraRezervacije = sifraRezervacije,
+                    Vreme = DateTime.Now.ToString()
+                };
+                entities.CheckIns.Add(novi);
+                entities.SaveChanges();
+
+            }
+            else
+            {
+                return View();
+            }
+
+            return View();
+
+        }
+
+    }//class
+}//namespace
