7ca785547f281a20475c25832924b506bd85bdb4
Nadica <nadica47317@its.edu.rs>
2020-02-04 14:40:35 +0100

    nedovrseno

--- AerConnect/Controllers/AccountController.cs
+++ AerConnect/Controllers/AccountController.cs
@@ -158,6 +158,7 @@ namespace AerConnect.Controllers
         public async Task<ActionResult> Register(RegisterViewModel model)
         {
             CustomPasswordHasher m = new CustomPasswordHasher();
+           
 
             if (ModelState.IsValid)
             {
@@ -172,31 +173,50 @@ namespace AerConnect.Controllers
 
 
                 };
-                entities.Putniks.Add(putnik);
-                entities.SaveChanges();
-                return View("Index");
+                if (entities.Putniks.Any(l => l.BrojPasosa.Equals(model.BrojPasosa)))
+                {
+                    return View("PostojeciPutnik");
+                    
+                }
+                else
+                {
+                   
+                    entities.Putniks.Add(putnik);
+                    entities.SaveChanges();
+                    return RedirectToAction("Index", "Home");
+                }
+                 
             }
-
+           
 
             var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-            var result = await UserManager.CreateAsync(user, model.Password);
-            if (result.Succeeded)
+          var result = await UserManager.CreateAsync(user, model.Password);
+            if (entities.Putniks.Any(l => l.BrojPasosa.Equals(model.BrojPasosa)))
             {
-                await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+                return View(model);
 
-                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                // Send an email with this link
-                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-
-                return RedirectToAction("Index", "Home");
             }
-            AddErrors(result);
-        
+            else
+            {
 
-            // If we got this far, something failed, redisplay form
-            return View(model);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+
+
+                // If we got this far, something failed, redisplay form
+                return View(model);
+            }
     }
 
 
