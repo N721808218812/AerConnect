a041959e5cced9a07090bd2ad0e7b69c8785d7e9
Nadica <nadica47317@its.edu.rs>
2020-02-04 13:52:36 +0100

    popravljen registacija

--- AerConnect/Controllers/AccountController.cs
+++ AerConnect/Controllers/AccountController.cs
@@ -25,10 +25,10 @@ namespace AerConnect.Controllers
         public AccountController()
         {
             entities = new AvioKompanijaEntities1();
-           
+
         }
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
         {
             UserManager = userManager;
             SignInManager = signInManager;
@@ -40,9 +40,9 @@ namespace AerConnect.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -128,7 +128,7 @@ namespace AerConnect.Controllers
             // If a user enters incorrect codes for a specified amount of time then the user account 
             // will be locked out for a specified amount of time. 
             // You can configure the account lockout settings in IdentityConfig
-            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent:  model.RememberMe, rememberBrowser: model.RememberBrowser);
+            var result = await SignInManager.TwoFactorSignInAsync(model.Provider, model.Code, isPersistent: model.RememberMe, rememberBrowser: model.RememberBrowser);
             switch (result)
             {
                 case SignInStatus.Success:
@@ -159,12 +159,10 @@ namespace AerConnect.Controllers
         {
             CustomPasswordHasher m = new CustomPasswordHasher();
 
-
-
-
             if (ModelState.IsValid)
             {
-                Putnik putnik = new Putnik {
+                Putnik putnik = new Putnik
+                {
                     Email = model.Email,
                     Password = m.HashPassword(model.Password),
                     Ime = model.Ime,
@@ -176,33 +174,31 @@ namespace AerConnect.Controllers
                 };
                 entities.Putniks.Add(putnik);
                 entities.SaveChanges();
-
+                return View("Index");
             }
-
-
-
 
 
             var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
-                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
-                    // Send an email with this link
-                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
-                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
-                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+            var result = await UserManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
-                    return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
+                // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                // Send an email with this link
+                // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                return RedirectToAction("Index", "Home");
             }
+            AddErrors(result);
+        
 
             // If we got this far, something failed, redisplay form
             return View(model);
-        }
+    }
+
 
         //
         // GET: /Account/ConfirmEmail
