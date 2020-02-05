eecd1dd2f1960fa8aa53863e36d9c070e2dc2f61
Nadica <nadica47317@its.edu.rs>
2020-02-05 19:45:02 +0100

    uradjen delete i napravljen folder za view Model

---
+++ AerConnect/Controllers/PassengerController.cs
@@ -0,0 +1,29 @@
+﻿using AerConnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AerConnect.Controllers
+{
+    public class PassengerController : Controller
+    {
+        private AvioKompanijaEntities1 entities1;
+
+        public PassengerController()
+        {
+            entities1 = new AvioKompanijaEntities1();
+        }
+        // GET: Passenger
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Search(Putnik putnik)
+        {
+            char 
+        }
+    }
+}
